6e3a773cda3d56d0413cd521d4717c87c991b2a5
mdc <de-marre@hotmail.com>
2023-02-19 16:41:58 +0100

    first commit

---
+++ src/main/java/com/swaggertake6/controller/controller.java
@@ -0,0 +1,12 @@
+package com.swaggertake6.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/sayHello")
+    public String SayHello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/swaggertake6/Swaggertake6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaggertake6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swaggertake6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
